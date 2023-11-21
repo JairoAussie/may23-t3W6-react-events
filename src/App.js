bc6fdc3b9f8a92519f0e8f0cab8ca9edd66a69e8
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-11-21 18:41:54 +1000

    create the app class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// Page that lets us increase or decrease 
+// how many Pokemon are on screen
+// App components needs to:
+// - Keep track of how many Pokemon to show
+// - iterate over a single, DRY modular component
+// - Button to increase Pokemon count
+// - Button to decrease Pokemon count
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+
+    // start with at least one pokemon on screen
+    this.state = {
+      pokemonCount: 1
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Pokemon page</h1>
+      </div>
+    )
+    
+  }
 }
 
 export default App;
